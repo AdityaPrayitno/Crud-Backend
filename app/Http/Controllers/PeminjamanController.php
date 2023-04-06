c9f12f73d3f1fa76b20a1e15538372fc50fe1454
AdityaPrayitno <jimjoker055@gmail.com>
2023-04-06 15:26:03 +0700

    Update PeminjamanController.php

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Peminjaman;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use illuminate\Support\carbon;
+use Illuminate\Support\carbon;
 
 class PeminjamanController extends Controller
 {
